1effe7c7ec0fbec565c142d98beb737f1e115595
Jared Brabazon <jbrabazon13@gmail.com>
2017-03-24 13:01:44 -0400

    added sloper files

---
+++ sloper/R/task9_simple_function.R
@@ -0,0 +1,11 @@
+#write a simple function
+
+slope <- function(x1, y1, x2, y2){
+  m <- ((y2-y1)/(x2-x1))
+  x <- x1
+
+  print(m)
+}
+
+#source('~/University/Classes/590_R/R/task9_simple_function.R')
+
